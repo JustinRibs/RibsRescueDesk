0986cad647ffc625cb368ea46139148200370666
Justin RIbarich <jribaric@nyit.edu>
2025-02-07 13:06:08 -0500

    favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/4230/4230745.png" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/png" sizes="167x167" href="https://cdn-icons-png.flaticon.com/512/4230/4230745.png"> 
+<link rel="apple-touch-icon" type="image/png" sizes="180x180" href="https://cdn-icons-png.flaticon.com/512/4230/4230745.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Ribs Rescue Desk</title>
 </head>
